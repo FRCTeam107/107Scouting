3bd30c7748baf269017fce38dd537daccdbc334d
David R <david.reidsma@gmail.com>
2019-07-23 13:15:21 -0400

    table work

--- app/src/main/java/com/frc107/scouting/form/Table.java
+++ app/src/main/java/com/frc107/scouting/form/Table.java
@@ -5,7 +5,7 @@ import android.util.SparseArray;
 import java.util.ArrayList;
 
 public class Table {
-    private SparseArray<Column> columns = new SparseArray<>();
+    private SparseArray<Column<Object>> columns = new SparseArray<>();
 
     public Table() {}
 
@@ -13,15 +13,23 @@ public class Table {
         if (columns.indexOfKey(id) == -1)
             throw new IllegalArgumentException("No column with id " + id);
 
-        Column col = columns.get(id);
+        Column<Object> col = columns.get(id);
+        if (value != null && !col.getTypeClass().equals(value.getClass()))
+            throw new IllegalArgumentException("Type of value must be the same as the type of column. value type: " + value.getClass().getName() + ", column type: " + col.getTypeClass().getName());
+
+        col.enterValue(value);
     }
 
     public class Column<T> {
+        private int id;
         private String name;
         private ArrayList<T> values = new ArrayList<>();
+        private Class<T> typeClass; // Used for typechecking
 
-        public Column(String name) {
+        public Column(int id, String name, Class<T> typeClass) {
+            this.id = id;
             this.name = name;
+            this.typeClass = typeClass;
         }
 
         public void enterValue(T value) {
@@ -32,8 +40,16 @@ public class Table {
             return values.get(row);
         }
 
+        public int getId() {
+            return id;
+        }
+
         public String getName() {
             return name;
+        }
+
+        public Class<T> getTypeClass() {
+            return typeClass;
         }
     }
 }
